8c69238a95794f2df24ec405c39b89ca870fc8be
Lee <hello@leejohnmartin.co.uk>
2024-08-16 19:49:47 +0100

    fix: Resolution

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,16 +1,13 @@
 using Godot;
 using System;
 
-public class Game : Node2D
+public class game : Node2D
 {
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
-
-	
-
 	public override void _Ready()
 	{
 		
