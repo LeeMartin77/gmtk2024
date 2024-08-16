30903ceb552459f6269649be5b0161a0b2b28482
Lee <hello@leejohnmartin.co.uk>
2024-08-16 20:16:47 +0100

    feat: Ticking, labels, expenses

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -7,15 +7,32 @@ public class Game : Node2D
     // private int a = 2;
     // private string b = "text";
 
+  	[Export]
+	public double TickRateSeconds = 1.0d;
+
+	private double _timeSinceLastTick = 0.0d;
+
+  	[Export]
+	public float IncomePerTick = 100.0f;
+  	[Export]
+	public float ExpensesPerTick = 110.0f;
+  	[Export]
+	public float Account = 10_000.0f;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _Process(float delta)
+	{
+		_timeSinceLastTick += delta;
+		if (_timeSinceLastTick > TickRateSeconds) {
+			_timeSinceLastTick -= TickRateSeconds;
+			Account += IncomePerTick;
+			Account -= ExpensesPerTick;
+		}
+	}
 }
---
+++ scripts/UI.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class UI : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+
+    private Game _game;
+
+    private Label _account;
+    private Label _income;
+    private Label _expenses;
+    public override void _Ready()
+    {
+    	_game = GetNode<Game>("/root/Root");
+
+        _account = GetNode<Label>("Funds");
+        _income = GetNode<Label>("Income");
+        _expenses = GetNode<Label>("Expenses");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        _account.Text = $"${_game.Account}";
+        _income.Text = $"${_game.IncomePerTick}";
+        _expenses.Text = $"${_game.ExpensesPerTick}";
+    }
+}
