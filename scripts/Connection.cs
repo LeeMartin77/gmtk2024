ab74c7fd04228465f6e1253bef607967fbb69d50
Lee <hello@leejohnmartin.co.uk>
2024-08-17 11:47:57 +0100

    feat: Packets move correctly

--- scripts/Connection.cs
+++ scripts/Connection.cs
@@ -13,12 +13,18 @@ public class Connection : Node2D
     private Contract _contract;
     private Game _game;
 
+    private Node2D _packetsIn;
+
     private PackedScene _packetScene;
+
+    private StaticBody2D _source;
 
     public async override void _Ready()
     {
         _packetScene = GD.Load<PackedScene>("res://Packet.tscn");
         _game = GetNode<Game>("/root/Root");
+        _packetsIn = GetNode<Node2D>("PacketsIn");
+        _source = GetNode<StaticBody2D>("Source");
         await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
         try {
 		    _contract = _game.Contracts.GetContractById(ContractId);
@@ -31,7 +37,10 @@ public class Connection : Node2D
     public void CreatePacket(float timeout) {
         var packet = _packetScene.Instance<Packet>();
         packet.TimeoutTime = timeout;
-        AddChild(packet);
+        var pktpos = _source.Position;
+        pktpos.y += 10;
+        packet.Position = pktpos;
+        _packetsIn.AddChild(packet);
     }
 
     
--- scripts/Packet.cs
+++ scripts/Packet.cs
@@ -8,17 +8,49 @@ public class Packet : RigidBody2D
     // private string b = "text";
 
     [Export]
-    public float TimeoutTime = 10.0f;
+    public double TimeoutTime = 10.0f;
+
+    public float MovementSpeed = 50.0f;
+
+
+    public bool Processed = false;
+
+    public Node2D Destination;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        Destination = GetParent().GetNode<Node2D>("../Destination");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+
+    public override void _Process(float delta)
+    {
+
+        TimeoutTime -= delta;
+
+        if (TimeoutTime <= 0) {
+            QueueFree();
+        }
+
+        base._Process(delta);
+    }
+
+    public override void _IntegrateForces(Physics2DDirectBodyState state)
+    {
+
+        if (Position.DistanceTo(Destination.Position) > 1.0f)
+        {        
+            LinearVelocity = Position.DirectionTo(Destination.Position) * MovementSpeed;
+        } else {
+            LinearVelocity = new Vector2();
+        }
+        base._IntegrateForces(state);
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 }
