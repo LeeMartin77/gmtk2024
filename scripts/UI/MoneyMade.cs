95bd7eb15118b560e0ba5b65103a11930f7e8197
Lee <hello@leejohnmartin.co.uk>
2024-08-19 16:26:24 +0100

    fix: Game over dialog

--- scripts/UI/MoneyMade.cs
+++ scripts/UI/MoneyMade.cs
@@ -18,6 +18,6 @@ public class MoneyMade : Label
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        Text = $"{_game.MoneyMade} MONEY MADE";
+        Text = $"£{_game.MoneyMade:#,###,##0} MADE\n";
     }
 }
